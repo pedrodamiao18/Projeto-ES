34fccc742613eafda80d932da3fe9df080e91a58
Ana Silva <ana.bd.silva@ubi.pt>
2025-12-10 15:42:55 +0000

    middleware de autenticação

---
+++ Middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware to verify JWT token
+const verifyJwt = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({error: "Token não fornecido"});
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            console.log("Erro de autenticação", err);
+            return res.status(401).json({error: "Token inválido"});
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+const authorizeRole = (requiredRole) => {
+    return (req, res, next) => {
+        if (!req.user || req.user.role !== requiredRole) {
+            return res.status(403).json({error: 'Acesso negado.'});
+        }
+        
+        next();
+    };
+};
+
+module.exports = {
+    verifyJwt,
+    authorizeRole
+};
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express');
+//vai ser para guardar variaveis sensiveis num ficheiro .env
+//o ficheiro .env nao deve ser posto to git
+require('dotenv').config();
+const express = require("express");
 const path = require('path');
+const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 3000;
+//const mongoose = require('mongoose'); MongoDB e Mongoose
 
+//MIDDLEWARE
 // Servir ficheiros estáticos da pasta 'public' (HTML, CSS, JS, imagens)
 app.use(express.static(path.join(__dirname, 'public')));
+//impede o browser de bloquear pedidos entre o frontend e o backend
+app.use(cors());
+//faz parse do json que vem no body do pedido
+//apenas se usarmos Content-Type : application/json
+app.use(express.json());
 
 // Página inicial
 app.get('/', (req, res) => {
@@ -11,7 +23,6 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor a correr em http://localhost:${PORT}`);
 });
